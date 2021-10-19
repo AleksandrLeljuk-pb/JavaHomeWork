55770ccf34b556f1a2d94c87bfd9b019e565329e
Aleksandr Leljuk <aleksandr.leljuk.01@privatbank.ua>
2021-10-20 02:49:57 +0300

    init commit

---
+++ src/com/pb/lelyuk/hw3/Bingo.java
@@ -0,0 +1,14 @@
+package com.pb.lelyuk.hw3;
+
+import java.util.Random;
+
+public class Bingo {
+    public static void main(String[] args) {
+        int randomNum=0;
+        double randomNum0;
+
+        randomNum0 = Math.random()*100;
+        System.out.println(randomNum0);
+
+    }
+}
