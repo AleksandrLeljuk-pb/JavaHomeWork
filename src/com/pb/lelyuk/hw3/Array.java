a5f2244bb32fe25a7a49e47ba4899402826a59a6
Aleksandr Leljuk <aleksandr.leljuk.01@privatbank.ua>
2021-10-24 18:32:41 +0300

    init commit

--- src/com/pb/lelyuk/hw3/Array.java
+++ src/com/pb/lelyuk/hw3/Array.java
@@ -1,5 +1,6 @@
 package com.pb.lelyuk.hw3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array {
@@ -8,6 +9,8 @@ public class Array {
         int counter = 0;
         int[] array = new int[10];
         int x = array.length;
+        int arraySum = 0;
+        int quantityPosNum = 0;
 
         System.out.println("Для створення масиву даних введіть 10 значень елементів масива (цілі числа)");
 
@@ -19,6 +22,33 @@ public class Array {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
+        for (int i = 0; i < array.length; i++) {
+            arraySum = arraySum + array[i];
+        }
+        System.out.println("Cума елементів масиву дорівнює " + arraySum);
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]>0) {
+                quantityPosNum++;
+            } else quantityPosNum = quantityPosNum+0;
+        }
+        System.out.println("Кількість позитивних елементів масиву дорівнює " + quantityPosNum);
+
+
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i] > array[i+1]){
+                    isSorted = false;
+
+                    buf = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
