894288e0e38ea2be17738fa1a7982829fceb4f3f
Aleksandr Leljuk <aleksandr.leljuk.01@privatbank.ua>
2021-10-30 17:03:20 +0300

    init commit

--- src/com/pb/lelyuk/hw4/CapitalLetter.java
+++ src/com/pb/lelyuk/hw4/CapitalLetter.java
@@ -6,33 +6,25 @@ import java.util.StringTokenizer;
 public class CapitalLetter {
 
     static String capitalFirst(String word) {
-        String word1 = word.substring(0,1);
+        String word1 = word.substring(0, 1);
         word = word.substring(1);
         word1 = word1.toUpperCase();
         word = word1 + word;
         return word;
     }
 
-
-
-
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String word = " ";
-        String sentense = "0";
         String sentense2 = " ";
 
-        sentense = scan.next();
-
+        System.out.println("Введіть речення для корегування");
+        String sentense = scan.nextLine();
         StringTokenizer sent = new StringTokenizer(sentense);
         while (sent.hasMoreElements()) {
-            sentense2 = sentense2 + capitalFirst(sent.nextToken());
-
+            sentense2 = sentense2 + " " + capitalFirst(sent.nextToken());
         }
-        System.out.println(sentense2 + ".");
+        System.out.println("відкореговане речення:");
+        System.out.println(sentense2);
 
-
-        System.out.println(word);
     }
 }
