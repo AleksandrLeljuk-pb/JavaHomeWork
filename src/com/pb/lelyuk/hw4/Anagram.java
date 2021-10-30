e8d1cba14c353d4b9afc53b46b325013fa8da5fb
Aleksandr Leljuk <aleksandr.leljuk.01@privatbank.ua>
2021-10-30 22:28:24 +0300

    init commit

--- src/com/pb/lelyuk/hw4/Anagram.java
+++ src/com/pb/lelyuk/hw4/Anagram.java
@@ -21,22 +21,15 @@ public class Anagram {
         String sentense2 = scan.nextLine();
 
         char[] sentens11 = sentense(sentense1);                //получаем из метода массив сформированный из 1 предложения
-        System.out.println(sentens11);
-        for (int i = 0; i < sentens11.length; i++) {
-            System.out.println(sentens11[i]);
-        }
 
         char[] sentens21 = sentense(sentense2);               //получаем из метода массив сформированный из 2 предложения
-        System.out.println(sentens21);
-        for (int i = 0; i < sentens21.length; i++) {
-            System.out.println(sentens21[i]);
-        }
+
         Boolean result = Arrays.equals(sentens11, sentens21);   //сравниваем два массива (условие: массив 1 равен массиву 2)
         System.out.println(result);
         if(result == true) {
             System.out.println("Друге речення є анаграмою першого речення");
         } else System.out.println("Друге речення НЕ Є анаграмою першого речення!");
-        //System.out.println("Final");
+
         }
 
 
