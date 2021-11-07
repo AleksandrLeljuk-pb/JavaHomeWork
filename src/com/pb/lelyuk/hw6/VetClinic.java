465544784bc867fbce70138d3eb5fe9fea417f39
Aleksandr Leljuk <aleksandr.leljuk.01@privatbank.ua>
2021-11-07 14:03:43 +0200

    Finish - deleted letter in cyrylic in the Name of MainClass "VetClinic" (letter "C")

--- src/com/pb/lelyuk/hw6/VetClinic.java
+++ src/com/pb/lelyuk/hw6/VetClinic.java
@@ -2,12 +2,12 @@ package com.pb.lelyuk.hw6;
 
 import java.lang.reflect.Constructor;
 
-public class VetСlinic {
+public class VetClinic {
     public static void main(String[] args) throws Exception {
         Animal[] animals = new Animal[3];
         animals[0] = new Dog("Pedigree", "Budka", "bark", "Pluto");
         animals[1] = new Cat("mice", "roof", "meow", "Tom");
-        animals[1] = new Horse("hay", "stable", "neigh", "Holstomer");
+        animals[2] = new Horse("hay", "stable", "neigh", "Holstomer");
 
         Class clazz = Class.forName("com.pb.lelyuk.hw6.Veterinarian");
         Constructor constr = clazz.getConstructor(new Class[]{});
