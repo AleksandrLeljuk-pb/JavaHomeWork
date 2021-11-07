b38054c257cbbbc30051317debf8a2cb96a78b68
Aleksandr Leljuk <aleksandr.leljuk.01@privatbank.ua>
2021-11-07 14:26:16 +0200

    Finish2 - Добавил в мейн класс проверку работы методов из первой части HW6

--- src/com/pb/lelyuk/hw6/VetClinic.java
+++ src/com/pb/lelyuk/hw6/VetClinic.java
@@ -18,6 +18,53 @@ public class VetClinic {
             }
         }
 
+// Проверка работы методов из первой части HW6:
+
+        Animal anim = new Animal("meet", "location");
+        anim.setAnimalName("Animal");
+        //anim.setFood("meet");
+
+        anim.makeNoise();
+        anim.sleep();
+        anim.eat();
+
+        Dog doggy = new Dog("Pedigree", "Budka", "bark", "Pluto");
+        doggy.setAnimalName("Dog");
+        //doggy.setFood("meet");
+        doggy.eat();
+        doggy.makeNoise();
+        doggy.sleep();
+        System.out.println(doggy.hashCode());
+        System.out.println("'" + doggy.getAnimalName() +"'" +  " равно 'мясо' - это " +doggy.equals("meet"));
+        System.out.println(doggy.toString());
+
+
+        Cat kitty = new Cat("mice", "roof", "meow", "Tom");
+        kitty.setAnimalName("Cat");
+        //kitty.setFood("meet");
+        kitty.eat();
+        kitty.makeNoise();
+        kitty.sleep();
+        System.out.println(kitty.hashCode());
+        System.out.println("'" + kitty.getAnimalName() +"'" +  " равно 'мясо' - это " +kitty.equals("meet"));
+        System.out.println(kitty.toString());
+
+        Horse horsy = new Horse("hay", "stable", "neigh", "Holstomer");
+        horsy.setAnimalName("Horse");
+        //horsy.setFood("meet");
+        horsy.eat();
+        horsy.makeNoise();
+        horsy.sleep();
+        System.out.println(horsy.hashCode());
+        System.out.println("'" + horsy.getAnimalName() +"'" +  " равно 'мясо' - это " +horsy.equals("meet"));
+        System.out.println(horsy.toString());
+
+        Veterinarian veter = new Veterinarian();
+        veter.treatAnimal(horsy);
+        veter.treatAnimal(kitty);
+        veter.treatAnimal(doggy);
+
+
 
 
     }
