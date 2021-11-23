fc0b90772cae4250eac56498981f40503a770600
Aleksandr Leljuk <aleksandr.leljuk.01@privatbank.ua>
2021-11-23 03:56:43 +0200

    Реализовано создание файла методом "createNumbersFile" в соответствии стребованиями ДЗ

--- src/com/pb/lelyuk/hw9/FileNumbers.java
+++ src/com/pb/lelyuk/hw9/FileNumbers.java
@@ -1,4 +1,4 @@
-package com.pb.lelyuk.hw9;
+//package com.pb.lelyuk.hw9;
 
 import java.io.BufferedWriter;
 import java.nio.file.Files;
@@ -6,25 +6,35 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileNumbers {
-
+//------------ метод создания файла -------------------
     static void createNumbersFile (Path path) {
 
         try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-            String a;
-            String b;
-            for (int i=0; i<10; i++) {
-                b = (int) ( Math.random() * 100);
-                a = a + b;
+            String b = new String();
+            int c;
+            for (int j=0; j<10; j++){
+                String a = new String();
+                for (int i = 0; i < 10; i++) {
+                    c = (int) (Math.random() * 100);
+                    a = a + " " + c;
+                }
+                b = b + a + System.lineSeparator();
             }
-            writer.write(String a);
+//            System.out.println(b); // (проверка построение файла в соответствии с требованиями ДЗ)
+
+            writer.write(b);
+        } catch (Exception ex) {
+            System.out.println("Error with file write: " + ex);
         }
+        System.out.println("Write to file \"" + path.toAbsolutePath() + "\" done!");
 
     }
 
     public static void main(String[] args) {
 
-        Path path = Paths.get("hw9/numbers.txt");
+        Path path = Paths.get("E:/Java/HW/numbers.txt");
 
+        createNumbersFile(path);
 
 
     }
